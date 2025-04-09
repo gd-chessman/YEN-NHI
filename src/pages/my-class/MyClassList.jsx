c688972d3649815af9b5edfa9d56480acf3685e5
Le Van Quy <vannn.quy@gmail.com>
2025-04-10 00:03:32 +0700

    yn

--- src/pages/my-class/MyClassList.jsx
+++ src/pages/my-class/MyClassList.jsx
@@ -140,7 +140,7 @@ export default function MyClassList() {
 
     const handleJoinClass = async () => {
         if (!joinClassId.trim()) {
-            toast.error('Please enter a class ID');
+            toast.error('Please enter a class code');
             return;
         }
 
